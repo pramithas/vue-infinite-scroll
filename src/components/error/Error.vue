a8808af4c1dec6a750498a7023b6a3262aab43d6
pramithas123 <pramithas@aruna.software>
2024-09-10 11:06:13 +0545

    Add error handling page

--- src/components/error/Error.vue
+++ src/components/error/Error.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>This is the error screen.</div>
-</template>
-
-<script setup lang="ts"></script>
+    <v-container class="d-flex flex-column align-center justify-center text-center">
+      <v-row>
+        <v-col>
+          <v-icon size="128" color="error" class="mb-4">mdi-alert-circle-outline</v-icon>
+          <p class="text-h4 font-weight-bold mb-3">Oops! Something went wrong</p>
+          <p class="text-body-1 mb-5">{{ errorMessage || defaultErrorMessage }}</p>
+  
+          <v-btn color="primary" @click="retryAction">
+            <v-icon left>mdi-refresh</v-icon> Retry
+          </v-btn>
+  
+          <v-btn color="secondary" class="ml-4" @click="goHome">
+            <v-icon left>mdi-home</v-icon> Go to Home
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  const props = defineProps({
+    errorMessage: {
+      type: String,
+      default: ''
+    }
+  });
+  
+  const defaultErrorMessage = 'An unexpected error occurred. Please try again later.';
+  
+  const router = useRouter();
+  
+  const retryAction = () => {
+    window.location.reload();
+  };
+  
+  const goHome = () => {
+    router.push('/');
+  };
+  </script>
+  
+  <style scoped>
+  .v-container {
+    height: 100vh;
+  }
+  </style>
+  
--- src/components/posts/PostList.vue
+++ src/components/posts/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-  <VErrorBoundary>
+  <VErrorBoundary @error-captured="handleError">
     <template #boundary="{ hasError }">
       <div v-if="!hasError">
         <p class="text-h3 mb-10 mt-4 text-center">Photo Collection App</p>
@@ -20,23 +20,31 @@
           </v-row>
         </InfiniteScroller>
       </div>
-      <div v-else>Message to appear if error occurred.</div>
+      <div v-else>
+        <Error />
+      </div>
     </template>
   </VErrorBoundary>
 </template>
 
 <script setup lang="ts">
 
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import VErrorBoundary from "vue-error-boundary";
 import usePhotos from '../../hooks/usePhotos';
 import Post from "./Post.vue";
 import InfiniteScroller from '../infinite-scroll/InfiniteScroller.vue';
+import Error from "../error/Error.vue";
 
 const { photos, loading, fetchPhotos, noMorePhotos } = usePhotos();
+
+const error = ref({});
+
+const handleError = (err: Error) => {
+  error.value = err.message;
+}
 
 onMounted(async () => {
   await fetchPhotos();
 });
-
 </script>
